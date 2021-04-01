fb5ec2ffffb5ce69bea7b2bc45fc6dcd7e4929c4
santiago-cortes-ortiz <jeissoncortes02@outlook.com>
2021-04-01 14:40:11 -0600

    Se implemento y completo la entidad tipos de cuentas con el crud completo

--- src/main/java/lyjstoreapirest/demo/tiposDeCuenta/infraestructure/controller/ControladorTiposDeCuenta.java
+++ src/main/java/lyjstoreapirest/demo/tiposDeCuenta/infraestructure/controller/ControladorTiposDeCuenta.java
@@ -1,15 +1,16 @@
 package lyjstoreapirest.demo.tiposDeCuenta.infraestructure.controller;
 
 import lombok.AllArgsConstructor;
+import lyjstoreapirest.demo.courier.domain.model.Courier;
 import lyjstoreapirest.demo.tiposDeCuenta.domain.model.TipoDeCuenta;
 import lyjstoreapirest.demo.tiposDeCuenta.domain.service.TiposDeCuentaServicio;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/tipodecuenta")
@@ -24,6 +25,53 @@ public class ControladorTiposDeCuenta {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Location","/api/v1/tipodecuenta/"+idTipoDeCuenta);
         return  new  ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<TipoDeCuenta>> listarTiposDeCuenta(){
+        return new ResponseEntity<>(tiposDeCuentaServicio.listarTiposDeCuenta(),HttpStatus.OK);
+    }
+
+    @GetMapping("/{idTipoDeCuenta}")
+    public ResponseEntity<TipoDeCuenta> buscarTipoDeCuentaPorId(@PathVariable("idTipoDeCuenta") Long idTipoDeCuenta){
+        Optional<TipoDeCuenta> tipoDeCuenta = tiposDeCuentaServicio.buscarTiposDeCuentaPorId(idTipoDeCuenta);
+        if (tipoDeCuenta.isEmpty()){
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Location","/api/v1/tipodecuenta/"+idTipoDeCuenta);
+            return new ResponseEntity<>(headers,HttpStatus.NOT_FOUND);
+        }
+        else{
+            return new ResponseEntity<>(tipoDeCuenta.get(),HttpStatus.OK);
+        }
+    }
+
+    @PutMapping("/actualizar/{idTipoDeCuenta}")
+    public ResponseEntity<Optional<TipoDeCuenta>> actualizarTipo(@RequestBody TipoDeCuenta nuevoTipo, @PathVariable("idTipoDeCuenta") Long idTipoDeCuenta){
+        Optional<TipoDeCuenta> tipoABuscar = tiposDeCuentaServicio.buscarTiposDeCuentaPorId(idTipoDeCuenta);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location","/api/v1/tipodecuenta/"+idTipoDeCuenta);
+        if (tipoABuscar.isEmpty()){
+            tiposDeCuentaServicio.guardar(nuevoTipo);
+            return new ResponseEntity<>(headers,HttpStatus.CREATED);
+        }
+        else{
+            tipoABuscar.get().setTipo(nuevoTipo.getTipo());
+            tiposDeCuentaServicio.guardar(tipoABuscar.get());
+            return new ResponseEntity<>(headers,HttpStatus.OK);
+        }
+    }
+
+    @DeleteMapping("/{idTipoDeCuenta}")
+    public  ResponseEntity<HttpStatus> eliminarTipoDeCuentaPorId(@PathVariable("idTipoDeCuenta") Long id){
+        Optional<TipoDeCuenta> tipoDeCuentaAEliminar = tiposDeCuentaServicio.buscarTiposDeCuentaPorId(id);
+        if(tipoDeCuentaAEliminar.isEmpty())
+        {
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add("Location", "/api/v1/tipodecuenta/"+id);
+            return new ResponseEntity<>(httpHeaders, HttpStatus.NOT_FOUND);
+        }
+        tiposDeCuentaServicio.eliminarTiposDeCuentaPorId(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
