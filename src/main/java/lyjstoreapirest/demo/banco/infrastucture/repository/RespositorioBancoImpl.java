dad640231db3d500cd14c78ae84110480c382da5
santiago-cortes-ortiz <jeissoncortes02@outlook.com>
2021-04-11 16:24:10 -0500

    diagramita picho

--- src/main/java/lyjstoreapirest/demo/banco/infrastucture/repository/RespositorioBancoImpl.java
+++ src/main/java/lyjstoreapirest/demo/banco/infrastucture/repository/RespositorioBancoImpl.java
@@ -5,14 +5,13 @@ import lyjstoreapirest.demo.banco.domain.model.Banco;
 import lyjstoreapirest.demo.banco.domain.repository.JpaMetodosBanco;
 import lyjstoreapirest.demo.banco.domain.repository.RespositorioBanco;
 import lyjstoreapirest.demo.general_service.Repositorio;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repositorio
 @AllArgsConstructor
-public class RespositorioBancoImpl implements RespositorioBanco {
+public class RespositorioBancoImpl implements RespositorioBanco  {
 
     private JpaMetodosBanco jpaMetodosBanco;
 
