29b80061425545ff7fc6bf7392ec314ae9118832
santiago-cortes-ortiz <jeissoncortes02@outlook.com>
2021-04-15 10:59:23 -0500

    se agrego listarBancos con dtos y algo desacopaldo?

--- src/main/java/lyjstoreapirest/demo/banco/infrastucture/RespositorioBancoImpl.java
+++ src/main/java/lyjstoreapirest/demo/banco/infrastucture/RespositorioBancoImpl.java
@@ -10,6 +10,7 @@ import lyjstoreapirest.demo.general_service.Repositorio;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Repositorio
 @AllArgsConstructor
@@ -37,6 +38,12 @@ public class RespositorioBancoImpl implements RespositorioBanco  {
 
     @Override
     public List<BancoDTO> listarBancos() {
-        return null;
+        List<Banco> listaBancoNormales = jpaMetodosBanco.findAll();
+        List<BancoDTO> listaBancoDto = listaBancoNormales.stream().map(listaDto -> BancoDTO.builder()
+                .id(listaDto.getId())
+                .nombre(listaDto.getNombre())
+                .build())
+                .collect(Collectors.toList());
+        return listaBancoDto;
     }
 }
